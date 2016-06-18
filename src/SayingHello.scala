50192b1f535223ba2ffca82510ff95031898237f
Philip Schwarz <pschwarz@expedia.com>
2016-06-18 18:23:30 +0100

    whitespace

--- src/SayingHello.scala
+++ src/SayingHello.scala
@@ -5,11 +5,8 @@ object SayingHello {
   def main(args: Array[String])
   {
     println("What is your name?")
-
     val name = StdIn.readLine()
-
     val greeting = s"Hello, $name, nice to meet you!"
-
     println(greeting)
   }
 }
